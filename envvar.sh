635219d7b8201764a20814307d15cb2bd063668f
Goodness Duru <goodyduru@gmail.com>
2024-06-06 17:19:23 +0100

    Add new env vars

--- envvar.sh
+++ envvar.sh
@@ -1,7 +1,5 @@
-#!/bin/bash
-set -o allexport
-. $(dirname "$0")/.env
-set +o allexport
+#!/bin/sh
+export $(cat $(dirname "$0")/.env | xargs) && env
 
 # Update .env.example just in case
 awk -F "=" '{print $1"="}' $(dirname "$0")/.env > $(dirname "$0")/.env.example
