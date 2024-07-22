f8b51d13774794c3d1c907e49580ad09e3413fc3
Goodness Duru <goodyduru@gmail.com>
2024-07-22 16:57:09 +0100

    Add toggle

--- src/post/static/post/js/script.js
+++ src/post/static/post/js/script.js
@@ -1,14 +1,31 @@
 function run() {
-    const toggles = document.querySelectorAll(".tog");
-    for ( let i = 0; i < toggles.length; i++  ) {
-        toggles[i].addEventListener("click", (e) => {
+    const toggles = document.getElementsByClassName("tog");
+    for ( toggle of toggles  ) {
+        toggle.addEventListener("click", (e) => {
             hideOrShow(e)
         });
     }
 }
 
 function hideOrShow(event) {
-    console.log(event.target)
+    let element = event.target;
+    let listId = "com_" + element.id;
+    let selectedElements = document.getElementsByClassName(element.id);
+    for ( child of selectedElements ) {
+        child.classList.toggle("noshow");
+    }
+    const main = document.getElementById(listId);
+    for ( const child of main.children ) {
+        if (child.tagName == "ARTICLE" || child.tagName == "FOOTER") {
+            child.classList.toggle("noshow");
+        }
+    }
+    let numComments = main.getAttribute("n");
+    if ( element.textContent.length == 3 ) {
+        element.textContent = "[" + numComments + " more]";
+    } else {
+        element.textContent = "[-]";
+    }
 }
 
 window.addEventListener("load", run);
